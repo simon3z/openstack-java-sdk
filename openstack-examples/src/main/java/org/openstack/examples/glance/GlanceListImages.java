be3b0f262c591008a8014e33231eb362e888efd7
Federico Simoncelli <fsimonce@redhat.com>
2013-03-21 16:45:52 +0100

    examples: add the GlanceListImages example
    This patch introduces a new example for the glance module. The example
    authenticates to a known keystone instance to query the catalog and
    retrieve the "image" (glance) services list.
    Then it connects to the first glance instance and retrieves the list of
    the available images.
    
    Signed-off-by: Federico Simoncelli <fsimonce@redhat.com>

---
+++ openstack-examples/src/main/java/org/openstack/examples/glance/GlanceListImages.java
@@ -0,0 +1,50 @@
+package org.openstack.examples.glance;
+
+import org.openstack.examples.ExamplesConfiguration;
+import org.openstack.glance.GlanceClient;
+import org.openstack.glance.api.ListImages;
+import org.openstack.glance.model.Image;
+import org.openstack.glance.model.Images;
+import org.openstack.keystone.KeystoneClient;
+import org.openstack.keystone.api.Authenticate;
+import org.openstack.keystone.api.ListTenants;
+import org.openstack.keystone.model.Access;
+import org.openstack.keystone.model.Tenants;
+import org.openstack.keystone.utils.KeystoneUtils;
+
+public class GlanceListImages {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		KeystoneClient keystone = new KeystoneClient(
+				ExamplesConfiguration.KEYSTONE_AUTH_URL);
+
+		Access access = keystone.execute(Authenticate.withPasswordCredentials(
+				ExamplesConfiguration.KEYSTONE_USERNAME,
+				ExamplesConfiguration.KEYSTONE_PASSWORD));
+
+		keystone.setToken(access.getToken().getId());
+		Tenants tenants = keystone.execute(new ListTenants());
+
+		if (tenants.getList().size() > 0) {
+			access = keystone.execute(Authenticate.withToken(
+					access.getToken().getId()).withTenantId(
+					tenants.getList().get(0).getId()));
+
+			GlanceClient glanceClient = new GlanceClient(
+					KeystoneUtils.findEndpointURL(access.getServiceCatalog(),
+							"image", null, "public"), access.getToken().getId());
+
+			Images images = glanceClient.execute(new ListImages(false));
+
+			for (Image image : images) {
+				System.out.println(image);
+			}
+		} else {
+			System.out.println("No tenants found!");
+		}
+	}
+
+}
